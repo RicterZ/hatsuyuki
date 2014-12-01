a9c341905d9c104e5c8b322286f7b6bc6fe624f2
ricterz <ricterzheng@gmail.com>
2014-12-01 17:55:25 +0800

    add LOGIN_URI and DEBUG

--- hatsuyuki/includes/config.sample.php
+++ hatsuyuki/includes/config.sample.php
@@ -4,3 +4,6 @@ define('DB_CONN', 'mongodb://localhost');
 define('DB_NAME', 'hatsuyuki');
 define('DB_USER', 'admin');
 define('DB_PASS', 'p1ssw0rd he3e');
+define('DEBUG', 1);
+
+$GLOBALS['LOGIN_URI'] = '/index.php/login/';
